c8ca64c972e15b71bd434885e0208f3056518721
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-14 18:51:50 +0100

    invert if condition

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -7,5 +7,5 @@ def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         return None
     else:
-        res = [[i + j for i, j in zip(arr1, arr2)]]
+        res = [i + j for i, j in zip(arr1, arr2)]
         return res
