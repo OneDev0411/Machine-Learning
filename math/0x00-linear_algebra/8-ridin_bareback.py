e6703d88fc6a47e37f1a620f2b0206ff97e579aa
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-14 23:46:10 +0100

    pure python

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -4,7 +4,14 @@ import numpy
 
 
 def mat_mul(mat1, mat2):
-    if len(mat1[0]) != len(mat2):
-        return None
-    npres = numpy.matmul(mat1, mat2)
-    return npres.tolist()
+    tmp = []
+    mul = []
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            dot = 0
+            for k in range(len(mat2)):
+                dot = dot + (mat1[i][k] * mat2[k][j])
+            tmp.append(dot)
+        mul.append(tmp)
+        tmp = []
+    return mul
