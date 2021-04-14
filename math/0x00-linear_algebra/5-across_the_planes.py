dde4e4709678b571bd8977d53c8dadea2ff6da3e
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-14 21:30:39 +0100

    empty matrix

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -5,7 +5,7 @@
 def add_matrices2D(mat1, mat2):
     """Add matrices element wise"""
     if mat1 == [[], []] or mat2 == [[], []]:
-        return None
+        return [[], []]
     elif matrix_shape(mat1) == matrix_shape(mat2):
         res = [[mat1[i][j] + mat2[i][j]
                 for j in range(len(mat1[0]))] for i in range(len(mat1))]
