398d5545eb9a95deaad89e4da9e09075c1088d21
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-14 18:32:34 +0100

    pep8 doc

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""add matrices element wise"""
+"""Add matrices element wise"""
 
 
 def add_matrices2D(mat1, mat2):
-    """add matrices element wise"""
+    """Add matrices element wise"""
     if matrix_shape(mat1) == matrix_shape(mat2):
         res = [[mat1[i][j]+mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
         return res
@@ -12,7 +12,7 @@ def add_matrices2D(mat1, mat2):
 
 
 def matrix_shape(matrix):
-    """add matrices element wise"""
+    """Determine matrix shape"""
     if not type(matrix) == list:
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
