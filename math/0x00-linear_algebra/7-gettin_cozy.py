0c0abb9e4f01cf8738fd8fdafaa14943ecefc9f9
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-14 23:28:43 +0100

    without numpy

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-import numpy as np
 """concat 2 matrices"""
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    if len(mat1[0]) != len(mat2):
-        return None
-    npmat1 = np.array(mat1)
-    npmat2 = np.array(mat2)
-    npres = np.concatenate((npmat1, npmat2), axis=axis)
-    res = npres.tolist()
-    return res
+    """concat 2 matrices along a specific axis"""
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        cat = zip(mat1, mat2)
+        return [i + j for i, j in cat]
+    else:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        cat = [res.copy() for res in mat1] + [res.copy() for res in mat2]
+        return cat
