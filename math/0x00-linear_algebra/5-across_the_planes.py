4c7cc25c680849f15ae3fe145983e88601dac324
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-14 21:28:18 +0100

    adding matrices are empty condition

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,7 +4,9 @@
 
 def add_matrices2D(mat1, mat2):
     """Add matrices element wise"""
-    if matrix_shape(mat1) == matrix_shape(mat2):
+    if mat1 == [[], []] or mat2 == [[], []]:
+        return None
+    elif matrix_shape(mat1) == matrix_shape(mat2):
         res = [[mat1[i][j] + mat2[i][j]
                 for j in range(len(mat1[0]))] for i in range(len(mat1))]
         return res
