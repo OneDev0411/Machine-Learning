d43b0ced312b3cf411908685076ddaf6e6d5c4e6
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-15 01:41:50 +0100

    without importing numpy

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """add, sub, mul and division element wise of np-arr"""
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
     """add, sub, mul and division element wise of np-arr"""
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
     return add, sub, mul, div
