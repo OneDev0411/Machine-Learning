e24e913afeaf7bf563eab4f314112b63e94844e3
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-20 02:27:25 +0100

    all in one

--- math/0x01-plotting/5-all_in_one.py
+++ math/0x01-plotting/5-all_in_one.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 y0 = np.arange(0, 11) ** 3
 
@@ -25,19 +26,48 @@ y32 = np.exp((r3 / t32) * x3)
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(2, 3)
+fig = plt.figure(figsize=(8, 8))
 fig.suptitle('All in One')
+gs = GridSpec(3, 2, figure=fig)
+
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[0, 1])
+ax3 = fig.add_subplot(gs[1, 0])
+ax4 = fig.add_subplot(gs[1, 1])
+ax5 = fig.add_subplot(gs[2, :])
+
+
 ax1.plot(y0, 'r-')
 ax1.set_xlim([0, 10])
 
 ax2.plot(x1, y1, 'm .')
-ax2.xlabel('Height (in)')
-ax2.ylabel('Weight (lbs)')
-ax2.title("Men's Height vs Weight")
-ax2.show()
+ax2.set_xlabel('Height (in)', fontsize='x-small')
+ax2.set_ylabel('Weight (lbs)', fontsize='x-small')
+ax2.set_title("Men's Height vs Weight", fontsize='x-small')
 
-ax3.plot()
+ax3.plot(x2, y2)
+ax3.set_xlabel('Time (years)', fontsize='x-small')
+ax3.set_ylabel('Fraction Remaining', fontsize='x-small')
+ax3.set_title("Exponential Decay of C-14", fontsize='x-small')
+ax3.set_yscale("log")
+ax3.set_xlim([0, 28650])
 
+ax4.plot(x3, y31, 'r--', label='C-14')
+ax4.plot(x3, y32, 'g-', label='Ra-226')
+ax4.legend(loc='upper right')
+ax4.set_xlabel('Time (years)', fontsize='x-small')
+ax4.set_ylabel('Fraction Remaining', fontsize='x-small')
+ax4.set_title("Exponential Decay of Radioactive Elements", fontsize='x-small')
+ax4.set_xlim([0, 20000])
+ax4.set_ylim([0, 1])
 
-ax4.plot()
-ax5.plot()
+ax5.hist(student_grades, bins=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100] , edgecolor='black')
+ax5.set_xlim([0, 100])
+ax5.set_ylim([0, 30])
+ax5.set_xticks(np.arange(0, 101, 10))
+ax5.set_xlabel('Grades', fontsize='x-small')
+ax5.set_ylabel('Number of Students', fontsize='x-small')
+ax5.set_title("Project A", fontsize='x-small')
+
+fig.tight_layout()
+plt.show()
