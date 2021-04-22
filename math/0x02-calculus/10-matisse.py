5751acc4eec49c6551aa1057a7d0402e6501be5a
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-22 21:19:20 +0100

    returns [0] when the derivative is 0

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Calculates the derivate of a polynomial"""
-
-
 def poly_derivative(poly):
     """Calculates the derivate of a poly"""
     if not isinstance(poly, list) or poly == []:
         return None
     deriv = [poly[i] * i for i in range(1, len(poly))]
+    if len(poly) == 1:
+        deriv = [0]
     return deriv
