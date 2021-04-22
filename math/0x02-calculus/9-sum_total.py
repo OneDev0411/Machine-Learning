de5e2851d89f1973f7c1fdf33b102fbf12e3ea10
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-22 18:56:10 +0100

    pep8

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """sum of i squared(n)"""
-
-
 def summation_i_squared(n):
-    if isinstance(n, int) and n >= 1:
-        return int((n * (n + 1) * (2 * n + 1)) / 6)
+    """sum of i squared of n"""
+    if type(n) == int and n >= 1:
+        return int((n*(n+1)*(2*n+1)) / 6)
     else:
         return None
