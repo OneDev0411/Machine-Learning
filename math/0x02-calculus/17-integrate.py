40824a75201c31f1abbd4135eeb48f2db554bccd
OneDev0411 <cryptoapple1109@gmail.com>
2021-04-22 23:34:01 +0100

    poly is [0] condition

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Integrate"""
-
-
 def poly_integral(poly, C=0):
     """ Calculate the integral of a polynomial """
-    if not isinstance(C, (float, int)):
+    if not isinstance(C, (float,int)):
         return None
     if not isinstance(poly, list) or poly == []:
         return None
+    if poly[0] == 0 and len(poly) == 1:
+        return [C]
     integral_poly = [C]
     for i in range(len(poly)):
-        if not isinstance(poly[i], (float, int)):
+        if not isinstance(poly[i],(float,int)):
             return None
-        a = poly[i] / (i + 1)
+        a = poly[i]/(i+1)
         if a % 1 == 0:
             integral_poly.append(int(a))
         else:
