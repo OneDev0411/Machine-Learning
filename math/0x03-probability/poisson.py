13fb8cef12d3550b7ac646f88cf0fa2901c5f7da
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-06 18:18:21 +0100

    Adding CDF instance method

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -32,3 +32,14 @@ class Poisson:
             for i in range(1, k + 1):
                 factorial_k = factorial_k * i
         return (e ** (- self.lambtha)) * (self.lambtha ** k) / factorial_k
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(0, k+1):
+            cdf += self.pmf(i)
+        return cdf
