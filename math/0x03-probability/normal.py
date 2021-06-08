8ac8de596c772126951819963c64e47a7b561c45
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-08 22:29:41 +0100

    adding PDF instance method

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -15,7 +15,7 @@ class Normal:
                 self.mean = float(mean)
                 self.stddev = float(stddev)
         else:
-            if type(data) != list:
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
             elif len(data) <= 2:
                 raise ValueError("data must contain multiple values")
@@ -32,3 +32,10 @@ class Normal:
     def x_value(self, z):
         """Calculates the x-value of a given z-score"""
         return (z * self.stddev) + self.mean
+
+    def pdf(self, x):
+        """Calculates the PDF of a given x-value"""
+        pi = 3.1415926536
+        e = 2.7182818285
+        return (1 / (self.stddev * ((2 * pi) ** 0.5))) * \
+            (e ** (-0.5 * (((x - self.mean) / self.stddev) ** 2)))
