b11977d9ba737c91a84aebc8abcd3b022597d841
OneDev0411 <cryptoapple1109@gmail.com>
2021-06-08 13:20:27 +0100

    adding PDF instance method

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -19,3 +19,10 @@ class Exponential:
             elif len(data) <= 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = 1 / (sum(data) / len(data))
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given time period"""
+        if x < 0:
+            return 0
+        e = 2.7182818285
+        return self.lambtha * (e ** (-self.lambtha * x))
