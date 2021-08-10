cf0f311a99d646153612dedaab9b023c0213dcef
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-11 00:36:17 +0100

    [Init Class] Privatize DeepNeuralNetwork instance attributes

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""a deep neural network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """nx is the number of input features to the neuron
+        layers is a list representing the number of nodes in each layer"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(self.__L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            if i == 0:
+                a = nx
+            else:
+                a = layers[i - 1]
+            self.__weights['W' + str(i + 1)
+                         ] = np.random.randn(layers[i], a) * np.sqrt(2 / a)
+            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
+
