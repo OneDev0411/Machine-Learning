91684eedb32eaec4bb5db23b097c77f7f9cb6fd5
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-11 22:55:10 +0100

    [Fix] Handling exceptions

--- supervised_learning/0x01-classification/26-deep_neural_network.py
+++ supervised_learning/0x01-classification/26-deep_neural_network.py
@@ -144,6 +144,8 @@ class DeepNeuralNetwork:
 
     def save(self, filename):
         """Saves the instance object to a file in pickle format"""
+        if not filename:
+            return None
         if not (filename.endswith('.pkl')):
             filename += '.pkl'
         with (open(filename, "wb+")) as file:
