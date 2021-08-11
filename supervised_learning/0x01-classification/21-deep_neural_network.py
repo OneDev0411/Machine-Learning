[Fix] Gradient descent

supervised_learning/0x01-classification/21-deep_neural_network.py
@@ -94,9 +94,10 @@ class DeepNeuralNetwork:
         self.__b1 += - alpha * ((np.sum(grad, axis=1, keepdims=True)) / m)"""
         grad = cache["A" + str(self.__L)] - Y
         for i in range(self.__L, 0, -1):
-            self.__weights["W" + str(i)] -= alpha * \
-                (np.dot(grad, cache["A" + str(i - 1)].T) / m)
-            self.__weights["b" + str(i)] -= alpha * \
-                (np.sum(grad, axis=1, keepdims=True) / m)
+            grada = np.matmul(grad, cache["A" + str(i - 1)].T) / m
+            gradb = np.sum(grad, axis=1, keepdims=True) / m
             grad = np.matmul(self.__weights["W" + str(i)].T, grad) * (
-                cache["A" + str(i - 1)] * (1 - cache["A" + str(i - 1)]))
+                    cache["A" + str(i - 1)] * (1 - cache["A" + str(i - 1)]))
+            self.__weights["W" + str(i)] -= alpha * grada
+            self.__weights["b" + str(i)] -= alpha * gradb
+