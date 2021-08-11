36e3f44b9d30eef1daefb2b398075d924feac5e2
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-11 15:12:24 +0100

    [Adding function] One-hot Encode

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""converts a numeric label vector into a one-hot matrix"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """Y is a numpy.ndarray containing numeric class labels
+    classes is the maximum number of classes found in Y"""
+    one_hot = np.zeros((classes, Y.max()+1))
+    one_hot[np.arange(classes), Y] = 1
+    return one_hot.T
