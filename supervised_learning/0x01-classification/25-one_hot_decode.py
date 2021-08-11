f2e7170c8e6cb7d0d968b71667f6f9e574bea878
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-11 20:20:33 +0100

    [Fix] Handling exceptions

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -5,7 +5,10 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """that converts a one-hot matrix into a vector of labels"""
+
     try:
+        if len(one_hot.shape) != 2:
+            return None
         inverted = np.argmax(one_hot.T, axis=1)
         return inverted
     except Exception:
