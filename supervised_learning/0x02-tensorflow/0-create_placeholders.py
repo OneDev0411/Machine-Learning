0c1d9885379fc5dcf12f515d563daa3afde2b926
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-19 12:46:19 +0100

    [Adding function] Placeholders

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
+    """x is the placeholder for the input data to the neural network,
+    y is the placeholder for the one-hot labels for the input data"""
     x = tf.placeholder("float", [None, nx], "x")
     y = tf.placeholder("float", [None, classes], "y")
     return x, y
