4aece4c48e7a014f16cd109c9184cffd0dee3d75
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-19 18:37:33 +0100

    [Adding function]Calculate Accuracy

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -6,5 +6,12 @@ import tensorflow as tf
 def calculate_accuracy(y, y_pred):
     """y is a placeholder for the labels of the input data
     y_pred is a tensor containing the network’s predictions"""
-    accuracy = tf.math.reduce_mean(tf.math.divide(y, y_pred, name=None), axis=None, keepdims=False, name=None)
+    accuracy = tf.math.reduce_mean(
+        tf.math.divide(
+            y,
+            y_pred,
+            name=None),
+        axis=None,
+        keepdims=False,
+        name=None)
     return accuracy
