8fa5b5ff2c1bae7653dd163029b9281f12397054
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-21 00:41:21 +0100

    [Adding function] Evaluate Output of NN

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -8,7 +8,7 @@ def evaluate(X, Y, save_path):
     Y is a numpy.ndarray containing the one-hot labels for X
     save_path is the location to load the model from"""
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path)
+        saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
