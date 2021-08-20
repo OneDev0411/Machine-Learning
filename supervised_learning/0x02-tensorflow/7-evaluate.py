894686ccf3998d307f99975caceb77ecdfe0b089
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-21 00:40:23 +0100

    [Adding function] Evaluate Output of NN

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -10,11 +10,11 @@ def evaluate(X, Y, save_path):
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(save_path)
         saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
         y_pred = tf.get_collection('y_pred')[0]
         loss = tf.get_collection('loss')[0]
         accuracy = tf.get_collection('accuracy')[0]
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
         y_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
         loss = sess.run(loss, feed_dict={x: X, y: Y})
         accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
