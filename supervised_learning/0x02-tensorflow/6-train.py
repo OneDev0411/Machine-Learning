433e8dec05caa22822137025af5e58960176c0b0
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-21 00:11:06 +0100

    [Adding function] Builds, trains, and saves a neural network classifier

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -31,13 +31,13 @@ def train(
     iterations is the number of iterations to train over
     save_path designates where to save the model"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    Y_pred = forward_prop(x, layer_sizes, activations)
-    loss = calculate_loss(y, Y_pred)
-    accuracy = calculate_accuracy(y, Y_pred)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    loss = calculate_loss(y, y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
     train_op = create_train_op(loss, alpha)
     tf.add_to_collection("x", x)
     tf.add_to_collection("y", y)
-    tf.add_to_collection("Y_pred", Y_pred)
+    tf.add_to_collection("y_pred", y_pred)
     tf.add_to_collection("loss", loss)
     tf.add_to_collection("accuracy", accuracy)
     tf.add_to_collection("train_op", train_op)
