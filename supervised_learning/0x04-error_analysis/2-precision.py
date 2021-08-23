b160e5938d361d3b90d2aa7b0343e3e4e8b14cb7
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-23 22:46:47 +0100

    [Adding function] Calculate Precision

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""calculates the precision for each class in a confusion matrix"""
+import numpy as np
+
+
+def precision(confusion):
+    """confusion is a numpy.ndarray where row indices
+    represent the correct labels and column indices
+    represent the predicted labels"""
+    return np.diag(confusion) / confusion.sum(axis=0)
