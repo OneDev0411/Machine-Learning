e8fff4b7555c6a13a57f76f83b6ab71f02c90d56
OneDev0411 <cryptoapple1109@gmail.com>
2021-08-24 23:43:14 +0100

    [Adding function] Normalization Constants

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""function that calculates the normalization constants of a matrix"""
+import numpy as np
+
+
+def normalization_constants(X):
+    """X is the numpy.ndarray of shape (m, nx) to normalize
+    m is the number of data points
+    nx is the number of features"""
+    m, nx = np.shape(X)
+    print(m, nx)
+    mean = np.zeros(nx,)
+    stdev = np.zeros(nx,)
+    for i in range(nx):
+        mean[i] = X[:, i].mean()
+        stdev[i] = X[:, i].std()
+    return mean, stdev
