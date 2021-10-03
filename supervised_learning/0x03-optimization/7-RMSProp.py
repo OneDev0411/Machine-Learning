cae097b966069e8bd23ba894403e55fb06ef832e
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-03 22:01:25 +0100

    [Adding function] RMSProp

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -7,5 +7,5 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """function that updates a variable using
     the RMSProp optimization algorithm"""
     s = s * beta2 + (1 - beta2) * (grad ** 2)
-    var = var - alpha * grad / epsilon + np.sqrt(s)
+    var = var - alpha * grad / (epsilon + np.sqrt(s))
     return var, s
