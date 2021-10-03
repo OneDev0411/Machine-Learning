d438fb086532a07de4d9b59605f718fe2fdcc5d3
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-04 00:46:23 +0100

    [Adding function] Learning rate decay

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -2,7 +2,6 @@
 """Learning rate decay"""
 
 
-
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """function that updates the learning rate using inverse time decay"""
     return alpha / (1 + (decay_rate * (global_step // decay_step)))
