79e99ccdec30f789cd17137fcd9eb31a1433f5e4
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-03 21:31:57 +0100

    [Adding function] momentum optimization

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ momentum optimization algorithm"""
 
+
 def update_variables_momentum(alpha, beta1, var, grad, v):
-    """updates a variable using the gradient descent with momentum optimization algorithm"""
+    """updates a variable using the gradient descent with
+    momentum optimization algorithm"""
     v = v * beta1 + (1 - beta1) * grad
     var = var - alpha * v
     return var, v
