b4ca6d0feb42b7b5d53d02329b88b2941fb02e87
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-06 10:27:33 +0100

    [Adding function] Dropout forward prop

---
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""function that that conducts forward propagation using Dropout"""
+import numpy as np
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """conducts forward propagation using Dropout"""
+    cache = {}
+    cache['A0'] = X
+    for i in range(L):
+        z = np.matmul(weights['W' + str(i + 1)], cache['A' + str(i)]) + weights['b' + str(i + 1)]
+        if i == L - 1:
+            x = np.exp(z)
+            cache['A' + str(i + 1)] = x / np.sum(x, axis=0)
+        else:
+            cache['A' + str(i + 1)] = np.tanh(z)
+            cache['D' + str(i + 1)] = np.random.binomial(1, keep_prob, size=z.shape)
+            cache['A' + str(i + 1)] *= cache['D' + str(i + 1)]
+            cache['A' + str(i + 1)] /= keep_prob
+    return cache
