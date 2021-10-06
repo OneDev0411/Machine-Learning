6f2c368701f330b2ea6f76cee2fbd4306c69e441
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-06 09:46:51 +0100

    [Adding function] L2 regularization cost (TF)

---
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""that calculates the cost of a neural network
+with L2 regularization"""
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    """cost is a tensor containing the cost of
+    the network without L2 regularization
+    Returns: a tensor containing the cost of the network accounting
+    for L2 regularization"""
+    return cost + tf.losses.get_regularization_losses(scope=None)
