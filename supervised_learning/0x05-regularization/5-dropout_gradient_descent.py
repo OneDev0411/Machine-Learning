309479c7be48b7b4945fb70bbac85c04df114089
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-07 03:23:47 +0100

    [Adding function] Dropout Gradient descent

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -11,9 +11,9 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     for i in range(L, 0, -1):
         dw = np.matmul(dz, cache["A" + str(i - 1)].T) / Y.shape[1]
         db = np.sum(dz, axis=1, keepdims=True) / Y.shape[1]
-        weights["W" + str(i)] -= alpha * dw
-        weights["b" + str(i)] -= alpha * db
         dz = np.matmul(weights["W" + str(i)].T, dz) * (
                 1 - cache["A" + str(i - 1)] * cache["A" + str(i - 1)])
         if i > 1:
             dz = (dz * cache["D" + str(i - 1)]) / keep_prob
+        weights["W" + str(i)] -= alpha * dw
+        weights["b" + str(i)] -= alpha * db
