3d02b53dcd4459a8cb688bbfcd0133fe7536bba9
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-09 08:15:40 +0100

    [Adding functions] keras Save & Load Model's Configuraton

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-"""functions that save and loads a model’s weights"""
+"""functions that save and loads a model’s configuration"""
 import tensorflow.keras as K
 
 
 def save_config(network, filename):
-    """network: the model whose weights should be saved
-    filename: path of the file that the weights should be saved to
-    save_format: format in which the weights should be saved
+    """network: the model whose configuration should be saved
+    filename: path of the file that the configuration should be saved to
     Returns: None
     """
     with open(filename, "w") as f:
@@ -15,7 +14,7 @@ def save_config(network, filename):
 
 def load_config(filename):
     """network: the model to which the weights should be loaded
-    filename is the path of the file that the weights should be loaded from
+    filename is the path of the file that the configuration should be loaded from
     Returns: the loaded model"""
     with open(filename, "r") as f:
         conf = f.read()
