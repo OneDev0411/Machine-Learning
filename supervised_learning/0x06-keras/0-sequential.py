377d265f587501cef315236bf3bc06f05db2f858
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-09 02:11:14 +0100

    [Adding function] keras Sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """function that builds a neural network with the Keras library"""
-from tensorflow import keras
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
@@ -10,14 +10,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     lambtha is the L2 regularization parameter
     keep_prob is the probability that a node will be kept for dropout
     Returns: the keras model"""
-    model = keras.Sequential()
+    model = K.Sequential()
     for i in range(len(layers)):
         if i != 0:
-            model.add(keras.layers.Dropout(1 - keep_prob))
+            model.add(K.layers.Dropout(1 - keep_prob))
         model.add(
-            keras.layers.Dense(
+            K.layers.Dense(
                 layers[i],
                 activation=activations[i],
-                kernel_regularizer=keras.regularizers.l2(lambtha),
+                kernel_regularizer=K.regularizers.l2(lambtha),
                 input_dim=nx))
     return model
