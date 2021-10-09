6878834cbb034c5da69a6628800bbf5a4063c0de
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-09 09:02:19 +0100

    [Adding function] Model prediction

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""functions that makes a prediction using a neural network:"""
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """network:  the network model to make the prediction with
+    data: input data to make the prediction with
+    verbose: boolean that determines if output should be
+    printed during the prediction process
+    Returns: prediction for the data
+    """
+    return network.predict(data, verbose=verbose)
