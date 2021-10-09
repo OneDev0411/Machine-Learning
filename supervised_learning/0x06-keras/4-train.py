1ec7b76ee3916ff82396a3cb53c07e618b730aa9
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-09 03:51:35 +0100

    [Adding function] keras Train model

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -3,14 +3,23 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(
+        network,
+        data,
+        labels,
+        batch_size,
+        epochs,
+        verbose=True,
+        shuffle=False):
     """network is the model to train
     data: array containing the input data
     labels: is a one-hot containing the labels of data
     batch_size: size of the batch used for mini-batch gradient descent
     epochs: the number of passes through data
-    verbose: boolean that determines if output should be printed during training
-    shuffle: boolean that determines whether to shuffle the batches every epoch.
+    verbose: boolean that determines if output should be
+    printed during training
+    shuffle: boolean that determines whether to shuffle the
+    batches every epoch.
     Returns: History object generated after training the model"""
     model = network
     hist_obj = model.fit(
