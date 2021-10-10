e66ff1aac025669a6ad43f669ce967d37e556285
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-10 03:37:38 +0100

    [Adding function] Valid Convolution

---
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""function that performs a valid convolution on grayscale images"""
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """images: numpy array with shape (m, h, w)
+    containing grayscale images
+    m: number of images
+    h: height in pixels of the images
+    w: width in pixels of the images
+    kernel: numpy array with shape (kh, kw)
+    containing the kernel for the convolution
+    Returns: numpy array containing the convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    out_h = h - kh + 1
+    out_w = w - kw + 1
+    conv = np.zeros(shape=(m, out_h, out_w))
+    for i in range(0, (out_h * out_w)):
+        row = i // out_w
+        col = i % out_h
+        conv[:, row, col] = (
+                images[:, row:kh+row, col:kw+col] * kernel).sum(axis=(1, 2))
+    return conv
---
+++ math/0x04-convolutions_and_pooling/README.md
@@ -0,0 +1,10 @@
+### Convolutions and Pooling
+- What is a convolution?
+- What is max pooling? average pooling?
+- What is a kernel/filter?
+- What is padding?
+- What is “same” padding? “valid” padding?
+- What is a stride?
+- What are channels?
+- How to perform a convolution over an image
+- How to perform max/average pooling over an image
