19de8603c5e5dc07a41af7be8817c6a444605304
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-11 06:05:20 +0100

    [Adding function] Same Convolution

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -16,17 +16,18 @@ def convolve_grayscale_same(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
     conv = np.zeros(shape=(m, h, w))
-    if kh % 2 == 1:
+    if kh % 2:
         pad_h = (kh - 1) // 2
     else:
         pad_h = kh // 2
-    if kw % 2 == 1:
+    if kw % 2:
         pad_w = (kw - 1) // 2
     else:
         pad_w = kw // 2
-    padded = np.pad(images, ((0,), (pad_h,), (pad_w,)), mode='constant')
+    padded = np.zeros(shape=(m, h + (pad_h * 2), w + (pad_w * 2)))
+    padded[:, pad_h:-pad_h, pad_w:-pad_w] = images
     for i in range(0, (h * w)):
-        row = i // h
+        row = i // w
         col = i % w
         conv[:, row, col] = (
             padded[:, row:kh + row, col:kw + col] * kernel).sum(axis=(1, 2))
