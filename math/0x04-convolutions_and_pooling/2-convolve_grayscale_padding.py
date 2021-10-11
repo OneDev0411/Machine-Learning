37d9f4d0468ee857f6d6067badea5294e8359f9b
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-11 07:25:22 +0100

    [Adding function] Custom Padding Convolution

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -23,8 +23,11 @@ def convolve_grayscale_padding(images, kernel, padding):
     out_h = (h + (pad_h * 2)) - kh + 1
     out_w = (w + (pad_w * 2)) - kw + 1
     conv = np.zeros(shape=(m, out_h, out_w))
-    padded = np.zeros(shape=(m, h + (pad_h * 2), w + (pad_w * 2)))
-    padded[:, pad_h:-pad_h, pad_w:-pad_w] = images
+    if pad_h == 0 and pad_w == 0:
+        padded = images
+    else:
+        padded = np.zeros(shape=(m, h + (pad_h * 2), w + (pad_w * 2)))
+        padded[:, pad_h:-pad_h, pad_w:-pad_w] = images
     for i in range(0, (out_h * out_w)):
         row = i // out_w
         col = i % out_w
--- math/0x04-convolutions_and_pooling/2-main.py
+++ math/0x04-convolutions_and_pooling/2-main.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
 import numpy as np
 convolve_grayscale_padding = __import__('2-convolve_grayscale_padding').convolve_grayscale_padding
 
+np.random.seed(2)
+m = np.random.randint(1000, 2000)
+h, w = np.random.randint(100, 200, 2).tolist()
+fh, fw = (np.random.randint(3, 10, 2)).tolist()
 
-if __name__ == '__main__':
-
-    dataset = np.load('../../supervised_learning/data/MNIST.npz')
-    images = dataset['X_train']
-    print(images.shape)
-    kernel = np.array([[1 ,0, -1], [1, 0, -1], [1, 0, -1]])
-    images_conv = convolve_grayscale_padding(images, kernel, (2, 4))
-    print(images_conv.shape)
-
-    plt.imshow(images[0], cmap='gray')
-    plt.show()
-    plt.imshow(images_conv[0], cmap='gray')
-    plt.show()
+images = np.random.randint(0, 256, (m, h, w))
+kernel = np.random.randint(0, 10, (fh, fw))
+conv_ims = convolve_grayscale_padding(images, kernel, (0, 0))
+print(conv_ims)
+print(conv_ims.shape)
