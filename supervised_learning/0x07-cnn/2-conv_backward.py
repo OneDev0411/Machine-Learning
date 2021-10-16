f5325106eebe113b91c3681d095c1289665fc4ad
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-16 22:27:44 +0100

    [Adding function] Convolutional Layer Backward Propagation

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -39,20 +39,28 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
         pad_w = (((w_prev - 1) * sw + kw - w_prev) // 2)
     else:
         pad_h, pad_w = 0, 0
-    A_prev = np.pad(
+    A_prev_pad = np.pad(
         A_prev, ((0,), (pad_h,), (pad_w,), (0,)), 'constant')
     dW = np.zeros(shape=(kh, kw, c_prev, c_new))
-    dA_prev = np.zeros_like(A_prev)
+    dA_prev_pad = np.zeros_like(A_prev_pad)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
     for im in range(m):
         for i in range(0, (h_new * w_new)):
             row = i // w_new
             col = i % w_new
             for kernel in range(c_new):
-                dW[:, :, :, kernel] += A_prev[
+                dW[:, :, :, kernel] += A_prev_pad[
                     im, row * sh:kh + row * sh, col * sw:kw + col * sw, :
                 ] * dZ[im, row, col, kernel]
-                dA_prev[im, row * sh:kh + row * sh, col * sw:kw + col *
-                        sw, :] += dZ[im, row, col, kernel] * W[:, :, :, kernel]
-    dA_prev = dA_prev[:, pad_h:h_prev + pad_h, pad_w:pad_w + w_prev, :]
+                dA_prev_pad[im,
+                            row * sh:kh + row * sh,
+                            col * sw:kw + col * sw,
+                            :] += dZ[im,
+                                     row,
+                                     col,
+                                     kernel] * W[:,
+                                                 :,
+                                                 :,
+                                                 kernel]
+    dA_prev = dA_prev_pad[:, pad_h:h_prev + pad_h, pad_w:pad_w + w_prev, :]
     return dA_prev, dW, db
