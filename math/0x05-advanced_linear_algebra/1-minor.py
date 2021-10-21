22e4c375a3d84cd376cccdb34420c9a52934a129
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-21 22:14:59 +0100

    [Adding function] Matrix Minor

---
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""function that calculates the minor of a matrix"""
+
+
+def determinant(matrix):
+    """matrix: list of lists whose determinant should be calculated"""
+    if matrix == [[]]:
+        return 1
+    elif not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a list of lists")
+    elif not all(isinstance(m, list) for m in matrix):
+        raise TypeError("matrix must be a list of lists")
+    elif not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+    width = len(matrix)
+    if width == 1:
+        return matrix[0][0]
+    else:
+        sign = -1
+        det = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            sign *= -1
+            det = det + sign * matrix[0][i] * determinant(m)
+    return det
+
+
+def minor(matrix):
+    """matrix: list of lists whose minr matrix should be calculated"""
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a list of lists")
+    elif not all(isinstance(m, list) for m in matrix):
+        raise TypeError("matrix must be a list of lists")
+    elif not all(len(row) == len(matrix) for row in matrix
+                 ) or len(matrix[0]) == 0:
+        raise ValueError("matrix must be a non-empty square matrix")
+    width = len(matrix)
+    if width == 1:
+        return [[1]]
+    minr = []
+    for i in range(width):
+        buff = []
+        for j in range(width):
+            sliced = [row[:] for row in matrix]
+            del sliced[i]
+            for col in sliced:
+                del col[j]
+            buff.append(determinant(sliced))
+        minr.append(buff)
+    return minr
