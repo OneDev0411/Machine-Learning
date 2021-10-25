85bb930d31c289c2b48a34093d9f1bda9c845ca3
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-25 23:52:05 +0100

    [Adding Method] PDF

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -5,6 +5,7 @@ import numpy as np
 
 class MultiNormal:
     """Class that represents a Multivariate Normal distribution"""
+
     def __init__(self, data):
         """data: numpy.ndarray of shape (d, n)
         n: number of data points
@@ -17,7 +18,7 @@ class MultiNormal:
         if n < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = data.mean(axis=1, keepdims=True)
-        self.cov = np.matmul(data-self.mean, (data-self.mean).T) / (n-1)
+        self.cov = np.matmul(data - self.mean, (data - self.mean).T) / (n - 1)
 
     def pdf(self, x):
         """public instance method  that calculates the PDF at a data point
@@ -27,8 +28,11 @@ class MultiNormal:
             raise TypeError("x must be a numpy.ndarray")
         if len(x.shape) != 2 or x.shape[1] != 1:
             raise TypeError("x must have the shape ({d}, 1)")
+        if x.shape[0] != self.cov.shape[0]:
+            raise TypeError("x must have the shape ({d}, 1)")
         d = x.shape[0]
         den = np.sqrt(((2 * np.pi) ** d) * np.linalg.det(self.cov))
         inv = np.linalg.inv(self.cov)
-        ex = (-0.5 * np.matmul(np.matmul((x - self.mean).T, inv), x - self.mean))
+        ex = (-0.5 * np.matmul(
+            np.matmul((x - self.mean).T, inv), x - self.mean))
         return (1 / den) * np.exp(ex[0][0])
