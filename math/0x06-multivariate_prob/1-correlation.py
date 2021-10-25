2b40ec5dfe49a0de17bea8b2811e2c49a67469ef
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-25 19:58:25 +0100

    [Adding function] Correlation

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Function that calculates a correlation matrix"""
+import numpy as np
+
+
+def correlation(C):
+    """ C: numpy.ndarray of shape (d, d)
+    d: number of dimensions"""
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise TypeError("C must be a 2D numpy.ndarray")
+    corr = np.outer(np.sqrt(np.diag(C)), np.sqrt(np.diag(C)))
+    return C / corr
