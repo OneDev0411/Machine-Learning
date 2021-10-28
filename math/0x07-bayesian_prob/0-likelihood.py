2e823cbe10c2ec50463220388cc829b7192a81d4
OneDev0411 <cryptoapple1109@gmail.com>
2021-10-28 22:47:33 +0100

    [Adding function] Likelihood

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" function that calculates the likelihood of obtaining
+ this data given various hypothetical probabilities
+ of developing severe side effects"""
+import numpy as np
+
+
+def factorial(x):
+    """ function that calculate the factorial of x """
+    fact = 1
+    for i in range(1, x + 1):
+        fact = fact * i
+    return fact
+
+
+def likelihood(x, n, P):
+    """ x: number of patients that develop severe side effects
+        n: the total number of patients observed
+        P: 1D numpy.ndarray containing the various hypothetical
+            probabilities of developing severe side effects """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is"
+                         "greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if (P > 0).all() or (P < 1).all():
+        raise ValueError("All values in P must be in "
+                         "the range [0, 1]")
+    return (factorial(n) / (
+            factorial(x) * factorial(n - x))) * (
+            P ** x) * ((1 - P) ** (n - x))
