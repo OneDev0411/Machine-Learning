62ecc7778b0de5dc5e928ea601c0f0544b2bef4f
OneDev0411 <cryptoapple1109@gmail.com>
2021-11-27 00:02:30 +0100

    [Adding function] Sparse autoencoder

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -31,9 +31,12 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     inpt_dec = keras.Input(shape=(latent_dims,))
     for i in hidden_layers[-2::-1]:
         if i == len(hidden_layers) - 1:
-            dec = (keras.layers.Dense(hidden_layers[i], activation="relu"))(inpt_dec)
+            dec = (
+                keras.layers.Dense(
+                    hidden_layers[-1],
+                    activation="relu"))(inpt_dec)
         else:
-            dec = keras.layers.Dense(hidden_layers[i], activation='relu')(dec)
+            dec = keras.layers.Dense(i, activation='relu')(dec)
     dec = keras.layers.Dense(input_dims, activation='sigmoid')(dec)
     decoder = keras.Model(inpt_dec, dec)
 
