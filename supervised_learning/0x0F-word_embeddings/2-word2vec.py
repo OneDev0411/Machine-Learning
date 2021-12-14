8b2e2e2a39edf61280c539a1082bc41482330847
OneDev0411 <cryptoapple1109@gmail.com>
2021-12-14 18:28:53 +0100

    [Adding function] Gensim Word2vec Model

---
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" A function that creates and trains a gensim word2vec model """
+from gensim.test.utils import common_texts
+from gensim.models import Word2Vec
+
+
+def word2vec_model(
+        sentences,
+        size=100,
+        min_count=5,
+        window=5,
+        negative=5,
+        cbow=True,
+        iterations=5,
+        seed=0,
+        workers=1):
+    """ sentences: list of sentences to be trained on
+        size: dimensionality of the embedding layer
+        min_count: minimum number of occurrences of a word
+                    for use in training
+        window: maximum distance between the current and
+                predicted word within a sentence
+        negative: size of negative sampling
+        cbow: boolean to determine the training type;
+              True is for CBOW; False is for Skip-gram
+        iterations: number of iterations to train over
+        seed: seed for the random number generator
+        workers: number of worker threads to train the model
+        Returns: the trained model"""
+    model = Word2Vec(
+        sentences=sentences,
+        size=size,
+        window=window,
+        min_count=min_count,
+        workers=workers,
+        negative=negative,
+        sg=cbow,
+        seed=seed)
+    model.train(sentences, total_examples=model.corpus_count, epochs=iterations)
+    return model
