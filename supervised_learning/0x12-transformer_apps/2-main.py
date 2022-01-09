82607007977bc767d0467209b26ecff5169c4dbb
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-09 18:27:44 +0100

    [Updating Class] Dataset

---
+++ supervised_learning/0x12-transformer_apps/2-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+Dataset = __import__('2-dataset').Dataset
+import tensorflow as tf
+
+data = Dataset()
+print('got here')
+for pt, en in data.data_train.take(1):
+    print(pt, en)
+for pt, en in data.data_valid.take(1):
+    print(pt, en)
