25b582352896143ec4eb1275fa10269d5519fa51
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-14 23:30:55 +0100

    [Adding Class] BidirectionalCell

---
+++ supervised_learning/0x0D-RNNs/5-bi_forward.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Class that represents a bidirectional cell of an RNN"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """ A class that represents a bidirectional cell of an RNN"""
+
+    def __init__(self, i, h, o):
+        """ i: dimensionality of the data
+            h: dimensionality of the hidden state
+            o: dimensionality of the outputs """
+        self.Whf = np.random.normal(0.0, 1.0, (i+h, h))
+        self.Whb = np.random.normal(0.0, 1.0, (i+h, h))
+        self.Wy = np.random.normal(0.0, 1.0, (2*h, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """ x_t: numpy.ndarray of shape (m, i)
+                 containing the data input for the cell
+                 m: batche size for the data
+            h_prev: numpy.ndarray of shape (m, h)
+                    containing the previous hidden state """
+        h_next = np.tanh(
+            np.matmul((
+                np.concatenate((h_prev, x_t), axis=1)), self.Whf) + self.bhf)
+        return h_next
