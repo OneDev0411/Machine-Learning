bee5d5460e810a376b2bbd22eeb537f384e68117
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-26 12:42:06 +0100

    [Adding function] Maximization Step

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -8,13 +8,16 @@ def maximization(X, g):
     """ X: numpy.ndarray of shape (n, d) containing the data set
         g: numpy.ndarray of shape (k, n) contains the posterior probability
         for each data point in each cluster"""
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2 or not isinstance(
-            g, np.ndarray) or len(g.shape) != 2 or X.shape[0] != g.shape[1]:
-        return None, None, None
-    pi = g.sum(axis=1) / X.shape[0]
-    m = np.dot(g, X) / g.sum(axis=1)[:, np.newaxis]
-    S = np.zeros((m.shape[0], m.shape[1], m.shape[1]))
-    for i in range(g.shape[0]):
-        S[i] = np.dot(((X - m[i]) * g[i, :, np.newaxis]).T, (X - m[
+    try:
+        if not isinstance(X, np.ndarray) or len(X.shape) != 2 or not isinstance(
+                g, np.ndarray) or len(g.shape) != 2 or X.shape[0] != g.shape[1]:
+            return None, None, None
+        pi = g.sum(axis=1) / X.shape[0]
+        m = np.dot(g, X) / g.sum(axis=1)[:, np.newaxis]
+        S = np.zeros((m.shape[0], m.shape[1], m.shape[1]))
+        for i in range(g.shape[0]):
+            S[i] = np.dot(((X - m[i]) * g[i, :, np.newaxis]).T, (X - m[
                 i])) / g.sum(axis=1)[i]
-    return pi, m, S
+        return pi, m, S
+    except Exception:
+        return None, None, None
