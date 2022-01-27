0514ad4e56d5d73f5edb11dfd33e996cb5b2c545
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-27 16:50:12 +0100

    [Adding function] Steady State

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -8,12 +8,18 @@ def regular(P):
     """ P: numpy.ndarray of shape (n, n) representing the transition matrix
         n: number of states in the markov chain """
     try:
+        if not np.all(np.isclose(P.sum(axis=1), 1)):
+            return None
         dim = P.shape[0]
+        steady = np.zeros((1, dim))
         q = (P-np.eye(dim))
         ones = np.ones(dim)
         q = np.c_[q,ones]
         QTQ = np.dot(q, q.T)
         bQT = np.ones(dim)
-        return np.linalg.solve(QTQ,bQT)
+        solve = np.linalg.solve(QTQ,bQT)
+        for i in range(dim):
+            steady[0][i] = solve[i]
+        return steady
     except Exception:
         return None
