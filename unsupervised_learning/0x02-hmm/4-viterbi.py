1e6f5c5ce7b75d05a0a8e43512c1dda4c9811a44
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-28 23:50:33 +0100

    [Adding function] Viterbi Algorithm

---
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" function that determines if a markov chain is absorbing """
+import numpy as np
+
+
+def viterbi(Observation, Emission, Transition, Initial):
+    """ Observation: numpy.ndarray of shape (T,)
+        Emission: numpy.ndarray of shape (N, M)
+        Transition: numpy.ndarray of shape (N, N)
+        Initial: numpy.ndarray of shape (N, 1)"""
+    T = Observation.shape[0]
+    N = Emission.shape[0]
+    viterbi = np.zeros((N, T))
+    vit = np.zeros((N, T))
+    viterbi[:, 0] = Initial.T * Emission[:, Observation[0]]
+    for i in range(1, T):
+        for j in range(N):
+            prob = viterbi[:, i-1] * Emission[
+                    j, Observation[i]] * Transition[:, j]
+            viterbi[j, i] = np.max(prob)
+            vit[j, i] = np.argmax(prob, 0)
+    path = [np.argmax(viterbi[:, T - 1])] + []
+    prev = np.argmax(viterbi[:, T - 1])
+    for t in range(T - 2, -1, -1):
+        prev = int(vit[prev, t])
+        path = [prev] + path
+    return path, np.max(viterbi[:, T - 1])
