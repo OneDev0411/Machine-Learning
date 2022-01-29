7276283e7aaeda9ac5762638c9b7154e3d330f82
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-29 13:10:31 +0100

    [Adding function] Baum_welch Algorithm

---
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" function that performs the Baum-Welch algorithm for a hidden markov model"""
+import numpy as np
+
+
+def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
+    """ Observation: numpy.ndarray of shape (T,)
+        Emission: numpy.ndarray of shape (N, M)
+        Transition: numpy.ndarray of shape (N, N)
+        Initial: numpy.ndarray of shape (N, 1)"""
+    pass
