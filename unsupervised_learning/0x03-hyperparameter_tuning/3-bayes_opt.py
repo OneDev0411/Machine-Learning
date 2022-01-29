0d2716c6b44d04559a23291c161d2fa7c59d9013
OneDev0411 <cryptoapple1109@gmail.com>
2022-01-29 20:06:15 +0100

    [Adding Class] Bayesian Optimization

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -9,7 +9,17 @@ class BayesianOptimization:
     """ A class that performs Bayesian optimization
     on a noiseless 1D Gaussian process"""
 
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(
+            self,
+            f,
+            X_init,
+            Y_init,
+            bounds,
+            ac_samples,
+            l=1,
+            sigma_f=1,
+            xsi=0.01,
+            minimize=True):
         """ f is the black-box function to be optimized
             X_init: numpy.ndarray of shape (t, 1)
             Y_init: numpy.ndarray of shape (t, 1)
@@ -23,6 +33,7 @@ class BayesianOptimization:
             performed for minimization (True) or maximization (False)"""
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], num=ac_samples).reshape(-1, 1)
+        self.X_s = np.linspace(
+            bounds[0], bounds[1], num=ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
