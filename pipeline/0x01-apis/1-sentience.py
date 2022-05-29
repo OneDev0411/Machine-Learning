8f6f089cf1c9b4875e4c9ffcdc97757e7161f4e0
OneDev0411 <cryptoapple1109@gmail.com>
2022-05-29 17:45:39 +0100

    [Adding method] Planets

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""method that returns the list of names
+of the home planets of all sentient species"""
+import requests
+
+
+def sentientPlanets():
+    """ returns the list of names of
+    the home planets of all sentient species"""
+    url = 'https://swapi-api.hbtn.io/api/species/'
+    planets = []
+    while url:
+        page = requests.get(url).json()
+        for result in page["results"]:
+            if result["designation"] == "sentient":
+                homeworld = result['homeworld']
+                if homeworld:
+                    planet = requests.get(homeworld).json()
+                    planets.append(planet["name"])
+        url = page["next"]
+    return planets
